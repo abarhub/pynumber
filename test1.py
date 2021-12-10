149ae86a393dea5d34ff19e2c0c669bdfce0f8e2
abarhub <abarhub@gmail.com>
2021-12-10 19:17:55 +0100

    fin du calcul

--- test1.py
+++ test1.py
@@ -4,43 +4,46 @@ from builtins import list
 
 class Variable:
     nom: str = ''
-    valeur:int = -1
+    valeur: int = -1
 
-    def __init__(self, nom:str, valeur:int):
+    def __init__(self, nom: str, valeur: int):
         self.nom = nom
         self.valeur = valeur
 
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return self.val()
 
-    def val(self)->str:
+    def val(self) -> str:
         if self.valeur >= 0:
             return str(self.valeur)
         else:
             return self.nom
 
-    def descr(self)->str:
+    def descr(self) -> str:
         return self.nom + '=' + str(self.valeur)
 
-    def __repr__(self)->str:
+    def __repr__(self) -> str:
         return self.__str__()
+
+    def copy(self):
+        return Variable(self.nom, self.valeur)
 
 
 class Constante:
-    valeur:int = 0
+    valeur: int = 0
 
-    def __init__(self, valeur:int):
+    def __init__(self, valeur: int):
         self.valeur = valeur
 
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return str(self.valeur)
 
-    def __repr__(self)->str:
+    def __repr__(self) -> str:
         return self.__str__()
 
 
 class ListVariable:
-    liste:list[Variable] = []
+    liste: list[Variable] = []
 
     def __str__(self):
         i = 0
@@ -52,7 +55,7 @@ class ListVariable:
             i = i + 1
         return '[' + s + ']'
 
-    def get(self, nom:str)->Variable:
+    def get(self, nom: str) -> Variable:
         for x in self.liste:
             if x.nom == nom:
                 return x
@@ -60,11 +63,11 @@ class ListVariable:
 
 
 class Mult:
-    x = None
-    y = None
-    ordre:int = 0
+    x: Variable = None
+    y: Variable = None
+    ordre: int = 0
 
-    def __init__(self, x, y, ordre:int):
+    def __init__(self, x: Variable, y: Variable, ordre: int):
         self.x = x
         self.y = y
         self.ordre = ordre
@@ -74,6 +77,17 @@ class Mult:
 
     def __repr__(self):
         return self.__str__()
+
+    def copy(self):
+        x1: Variable = None
+        y1: Variable = None
+        if self.x != None:
+            x1 = self.x.copy()
+        if self.y != None:
+            y1 = self.x.copy()
+        m: Mult = Mult(x1, y1, self.ordre)
+        return m
+
 
 # class Equation:
 #     list = []
@@ -92,8 +106,8 @@ class Mult:
 
 
 class MultiplicationComplete:
-    liste:list[Mult] = []
-    valeurs:list[int] = []
+    liste: list[Mult] = []
+    valeurs: list[int] = []
 
     def __str__(self):
         i = 0
@@ -106,8 +120,34 @@ class MultiplicationComplete:
             i = i + 1
         return s + '(' + str(self.valeurs) + ')'
 
-    def getByOrder(self,ordre:int)->list[Mult]:
-        return [x for x in self.liste if x.ordre<=ordre]
+    def __repr__(self):
+        return self.__str__()
+
+    def getByOrder(self, ordre: int) -> list[Mult]:
+        return [x for x in self.liste if x.ordre <= ordre]
+
+    def calcul(self, ordre: int) -> int:
+        res: int = 0
+        for i in range(0, ordre + 1):
+            mult: Mult = self.liste[i]
+            if mult.x.valeur == -1 or mult.y.valeur == -1:
+                return -1
+            res += mult.x.valeur * mult.y.valeur * 10 ** i
+        return res
+
+    def valeur(self, ordre: int) -> int:
+        res: int = 0
+        for i in range(0, ordre + 1):
+            v: int = self.valeurs[i]
+            res += v * 10 ** i
+        return res
+
+    def copy(self):
+        res: MultiplicationComplete = MultiplicationComplete()
+        res.valeurs = [x for x in self.valeurs]
+        res.liste = [x.copy() for x in self.liste]
+        return res
+
 
 #                   A  B  C  D
 #                         E  F
@@ -120,7 +160,7 @@ class MultiplicationComplete:
 #
 
 
-def construit(numberList:list[str])->ListVariable:
+def construit(numberList: list[str]) -> ListVariable:
     i = 0
     list = ListVariable()
     while i < len(numberList):
@@ -137,15 +177,15 @@ def construit(numberList:list[str])->ListVariable:
     return list
 
 
-def construitEquation(numberList :list[str], listeVariables: list)-> MultiplicationComplete:
-    nb = len(numberList)
-    nb2 = int(math.ceil(nb / 2))
+def construitEquation(numberList: list[str], listeVariables: ListVariable) -> MultiplicationComplete:
+    nb: int = len(numberList)
+    nb2: int = int(math.ceil(nb / 2))
     # nb = 1
     # nb2 = 1
-    liste = MultiplicationComplete()
+    liste: MultiplicationComplete = MultiplicationComplete()
     liste.valeurs = [int(x) for x in reversed(numberList)]
-    for x in range(1, nb2):
-        for y in range(1, nb):
+    for x in range(1, nb + 1):
+        for y in range(1, nb2 + 1):
             varx = listeVariables.get('x' + str(x))
             vary = listeVariables.get('y' + str(y))
             if varx is not None and vary is not None:
@@ -156,47 +196,125 @@ def construitEquation(numberList :list[str], listeVariables: list)-> Multiplicat
                 liste.liste.append(tmp2)
     return liste
 
-def inList(liste:list[Variable], nom:str)-> bool:
+
+def inList(liste: list[Variable], nom: str) -> bool:
     for x in liste:
-        if x.nom==nom:
+        if x.nom == nom:
             return True
     return False
 
-def getVariables(liste:list[Mult])->list[Variable]:
-    liste2:list[Variable]=[x.x for x in liste if x.x!=None and x.x.valeur>=0]
-    liste3:list[Variable] = [x.y for x in liste if x.y != None and x.y.valeur>=0]
-    liste4:list[Variable]=[]
-    liste4.append(liste2)
-    liste4.append(liste3)
-    liste5:list[str]=[x.nom for x in liste4]
-    set=set(liste5)
-    list6:list[Variable]=[]
+
+def getVariables(liste: list[Mult]) -> list[Variable]:
+    liste2: list[Variable] = [x.x for x in liste if x.x != None and x.x.valeur == -1]
+    liste3: list[Variable] = [x.y for x in liste if x.y != None and x.y.valeur == -1]
+    liste4: list[Variable] = []
+    liste4.extend(liste2)
+    liste4.extend(liste3)
+    liste5: list[str] = [x.nom for x in liste4]
+    set2 = set(liste5)
+    list6: list[Variable] = []
     for x in liste4:
-        if x.nom in set:
-            if not inList(list6,x.nom):
+        if x.nom in set2:
+            if not inList(list6, x.nom):
                 list6.append(x)
     return list6
 
 
-def resolution(eq:MultiplicationComplete):
-    for i in range(0, int(math.ceil(len(eq.valeurs) / 2))):
-        tmp = eq.getByOrder(i)
-        print("ordre", str(i), str(tmp))
+def incr(tab: list[int]) -> list[int]:
+    len2 = len(tab)
+    tab2: list[int] = tab.copy()
+    pos = len2 - 1
+    while pos >= 0:
+        v = tab2[pos]
+        if v < 9:
+            v = v + 1
+            tab2[pos] = v
+            break
+        else:
+            tab2[pos] = 0
+            pos = pos - 1
+    if pos < 0:
+        return []
+    else:
+        return tab2
 
-        listVariables:list[Variable]=getVariables(tmp)
-        for v in listVariables:
-            for x in range(0, 10):
-                v.valeur=x
-            v.valeur=-1
+
+def listValue(n: int) -> list[list[int]]:
+    res: list[list[int]] = []
+    res2: list[int] = [0 for _ in range(0, n)]
+    res.append(res2)
+
+    res3: list[int] = res2.copy()
+    while True:
+        res3 = incr(res3)
+        if len(res3) == 0:
+            break
+        else:
+            res.append(res3)
+    return res
 
 
+def estValide(eq: MultiplicationComplete, ordre: int) -> bool:
+    res = eq.calcul(ordre)
+    if res == -1:
+        return False
+    res2 = eq.valeur(ordre)
+    return (res - res2) % (10 ** (ordre + 1)) == 0
 
 
+def resolution2(eq: MultiplicationComplete, ordre: int, max: int, listResultat: list[MultiplicationComplete]):
+    tmp = eq.getByOrder(ordre)
+    print("ordre", str(ordre), str(tmp))
 
-n='28741'
-#list = ['2', '8', '7', '4', '1']
-list=[char for char in n]
-listeVariables = construit(list)
+    # if len(tmp)==0:
+    #     print("eq", str(eq))
+    #     return
+
+    listVariables: list[Variable] = getVariables(tmp)
+
+    listValeur: list[list[int]] = listValue(len(listVariables))
+
+    print("listVariables", str(listVariables))
+    print("listValeur", str(listValeur))
+    for val in listValeur:
+        # affectation des valeurs
+        for i in range(0, len(val)):
+            v = listVariables[i]
+            v.valeur = val[i]
+        if estValide(eq, ordre):
+            if ordre + 1 < max:
+                resolution2(eq, ordre + 1, max, listResultat)
+            else:
+                listResultat.append(eq.copy())
+                print("eq", str(eq))
+        for i in range(0, len(val)):
+            v = listVariables[i]
+            v.valeur = -1
+
+
+def resolution(eq: MultiplicationComplete):
+    # resolution2(eq, 0, int(math.ceil(len(eq.valeurs) / 2)))
+    res: list[MultiplicationComplete] = []
+    resolution2(eq, 0, len(eq.valeurs), res)
+    print('resultat:', str(res))
+    # for i in range(0, int(math.ceil(len(eq.valeurs) / 2))):
+    #     tmp = eq.getByOrder(i)
+    #     print("ordre", str(i), str(tmp))
+    #
+    #     listVariables: list[Variable] = getVariables(tmp)
+    #
+    #     listValeur = listValue(len(listVariables))
+    #
+    #     print("listVariables", str(listVariables))
+    #     print("listValeur", str(listValeur))
+
+
+# n = '28741'
+n = '21'
+# n = '115'
+# list = ['2', '8', '7', '4', '1']
+list = [char for char in n]
+listeVariables: ListVariable = construit(list)
 
 print("var", str(listeVariables))
 
@@ -204,14 +322,10 @@ eq = construitEquation(list, listeVariables)
 
 print("eq", str(eq))
 
-list2=eq.getByOrder(0)
+list2 = eq.getByOrder(0)
 
 print("list2", str(list2))
 
 print("ordre1", str(eq.getByOrder(1)))
 
-
 resolution(eq)
-
-
-
