2ce72b5e2b5e3ccd9e3d078bdd5b67a4f984d932
abarhub <abarhub@gmail.com>
2022-08-26 10:50:50 +0200

    reformatage du code

--- genere_premier.py
+++ genere_premier.py
@@ -11,7 +11,7 @@ def isprime(number: int) -> bool:
     return True
 
 
-def listePrime(min:int ,max: int, resultat):
+def listePrime(min: int, max: int, resultat):
     for x in range(min, max):
         if isprime(x):
             resultat(x)
@@ -21,28 +21,25 @@ def noop(x: int):
     pass
 
 
-def listePrimeSansAffichage(min:int,max: int, f):
-    listePrime(min,max, lambda x: f.write(str(x)+"\n"))
+def listePrimeSansAffichage(min: int, max: int, f):
+    listePrime(min, max, lambda x: f.write(str(x) + "\n"))
 
 
 def main():
-    min=2
-    #max: int = 100
-    #max: int = 50000
-    max:int=100000
+    min = 2
+    max: int = 100
+    # max: int = 50000
+    # max:int=100000
 
-    with open('files/primes_'+str(min)+'_'+str(max)+'.txt', "w") as f:
-
-
+    with open('files/primes_' + str(min) + '_' + str(max) + '.txt', "w") as f:
         start = time.time()
 
-        listePrimeSansAffichage(min,max,f)
+        listePrimeSansAffichage(min, max, f)
 
         end = time.time()
         elapsed = end * 1000 - start * 1000
 
         print(f'Temps d\'exécution : {elapsed}ms')
-
 
 
 if __name__ == '__main__':
